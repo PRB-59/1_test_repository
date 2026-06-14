8d5235c54ac4361e18eaef1ecbab23613a6926b2
PRB-59 <21ec06003@iitbbs.ac.in>
2026-06-14 18:14:59 +0530

    Added fries if the user feels hungry

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ hungry = input("Enter your hungry status (yes/no): ")
 if hungry=="yes":
     print("Eat samosa!")
     print("Eat burger!")
+    print("Eat fries!")
     print("Eat pizza as well!")
 else:
     print("Do your homework.")
