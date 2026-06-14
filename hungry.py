79fb911b51ab1fc8552afdf739ae05fc51983e0e
PRB-59 <21ec06003@iitbbs.ac.in>
2026-06-14 17:25:03 +0530

    Added burger as well. But will perform undo.

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry = input("Enter your hungry status (yes/no): ")
 
 if hungry=="yes":
     print("Eat samosa!")
+    print("Eat burger!")
     print("Eat pizza as well!")
 else:
     print("Do your homework.")
