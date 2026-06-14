c3319297c19fa059d06a68d19718ea0bce1f39b8
PRB-59 <21ec06003@iitbbs.ac.in>
2026-06-14 17:25:31 +0530

    Revert "Added burger as well. But will perform undo."
    This reverts commit 79fb911b51ab1fc8552afdf739ae05fc51983e0e.

--- hungry.py
+++ hungry.py
@@ -2,7 +2,6 @@ hungry = input("Enter your hungry status (yes/no): ")
 
 if hungry=="yes":
     print("Eat samosa!")
-    print("Eat burger!")
     print("Eat pizza as well!")
 else:
     print("Do your homework.")
