1ae99030abe9914312410573d3dd3552269a8db6
PRB-59 <21ec06003@iitbbs.ac.in>
2026-06-14 22:28:36 +0530

    Moved to 1 commit back, and did change in the code at that commit

--- hungry.py
+++ hungry.py
@@ -4,5 +4,6 @@ if hungry=="yes":
     print("Eat samosa!")
     print("Eat burger!")
     print("Eat pizza as well!")
+    print("And then stop eating zunk food.")
 else:
     print("Do your homework.")
